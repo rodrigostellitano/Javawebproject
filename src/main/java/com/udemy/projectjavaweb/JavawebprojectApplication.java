837e7fa212346fd110ba80b28e4ff053a0292daf
Rodrigo Stellitano <rodrigostellitano@gmail.com>
2020-07-09 13:10:04 -0300

    Aula 1 - Criou Controller, importou html css e precisei trocar a porta no application.properties para 8181

---
+++ src/main/java/com/udemy/projectjavaweb/JavawebprojectApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.projectjavaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavawebprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavawebprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udemy/projectjavaweb/JavawebprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.projectjavaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavawebprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
